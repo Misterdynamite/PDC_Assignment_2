c9769ddaeb118b863ee32691adcda02488eb6076
Misterdynamite <misterdynamite112@gmail.com>
2025-05-23 20:51:59 +1200

    Initial commit

---
+++ src/main/java/com/aut603/pdc_assignment_2/PDC_Assignment_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.aut603.pdc_assignment_2;
+
+/**
+ *
+ * @author Patchouli
+ */
+public class PDC_Assignment_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
